935e17595c5d26565483a5723b4d5ea0f8f3420a
Francis <francisuk1989@gmail.com>
2024-03-31 15:00:17 +0100

    get the weather info from text files

--- send-environment-metrics.py
+++ send-environment-metrics.py
@@ -1,23 +1,41 @@
 from meshtastic import portnums_pb2, telemetry_pb2
 
 # For connection over serial
-import meshtastic.serial_interface
-interface = meshtastic.serial_interface.SerialInterface()
+# import meshtastic.serial_interface
+# interface = meshtastic.serial_interface.SerialInterface()
 
 # For connection over TCP
-# import meshtastic.tcp_interface
-# interface = meshtastic.tcp_interface.TCPInterface(hostname='192.168.1.42', noProto=False)
+import meshtastic.tcp_interface
+interface = meshtastic.tcp_interface.TCPInterface(hostname='192.168.2.89', noProto=False)
 
-BROADCAST_ADDR = 4294967295
+# NODE ID to send the weather infomation
+BROADCAST_ADDR = XXXXXX
 
+# Grab the Weather info
+file1 = open('AirQuality')
+sendAirQuality = file1.readlines()
+file2 = open('Humidity')
+sendHumidity = file2.readlines()
+file3 = open('Pressure')
+sendPressure = file3.readlines()
+file4 = open('Temperature')
+sendTemperature = file4.readlines()
+
+# Send data to the node
 telemetry_data = telemetry_pb2.Telemetry()
-telemetry_data.environment_metrics.temperature = 352.222
-telemetry_data.environment_metrics.relative_humidity = 69
-telemetry_data.environment_metrics.barometric_pressure = 0
-telemetry_data.environment_metrics.gas_resistance = 0
-telemetry_data.environment_metrics.voltage = 0
-telemetry_data.environment_metrics.current = 0
 
+for line in sendTemperature:
+    print("telemetry_data.environment_metrics.temperature = {}".format(line.strip()))
+
+for line in sendHumidity:
+    print("telemetry_data.environment_metrics.relative_humidity = {}".format(line.strip()))
+
+for line in sendPressure:
+    print("telemetry_data.environment_metrics.barometric_pressure = {}".format(line.strip()))
+
+for line in sendAirQuality:
+    print("telemetry_data.environment_metrics.gas_resistance = {}".format(line.strip()))
+    
 interface.sendData(
     telemetry_data,
     destinationId=BROADCAST_ADDR,
